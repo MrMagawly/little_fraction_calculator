6e1cd1a7e950b79b0aaa2c043037e7f9cd7f2291
MrMagawly <mr_magawly@protonmail.com>
2022-02-25 15:53:37 -0500

    Initial commit.

---
+++ fractions.py
@@ -0,0 +1,18 @@
+class fraction():
+    def __init__(self,n,d):
+        self.n = n
+        self.d = d
+        self.reduce()
+
+    def __add__(self, fr):
+        return fraction(self.n * fr.d + fr.n * self.d, self.d * fr.d)
+
+    def __repr__(self):
+        return 'fraction({0}, {1})'.format(self.n, self.d)
+
+    def __str__(self):
+        return '{0}/{1}'.format(self.n, self.d)
+
+    def reduce(self):
+        pass
+
