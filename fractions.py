ae011962d714c887710b265390bedb65b6fe95f7
MrMagawly <mr_magawly@protonmail.com>
2022-03-16 18:37:53 -0400

    A step of negative one was added to the step of the for loop in reduce() method.

--- fractions.py
+++ fractions.py
@@ -48,7 +48,7 @@ class fraction():
             n = 1
             d = 1
         else:
-            for i in range(gap, 1):
+            for i in range(gap, 1, -1):
                 if n % gap == 0 and d % gap == 0:
                     n /= gap
                     d /= gap
